47ab58aa0cee3ed0ec9f09a115ce1ea9e1244efd
Younggyu <yonggyu1234@naver.com>
2023-10-10 23:23:16 +0900

    [Bronze II] Title: 소수 찾기, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

---
+++ 백준/Bronze/1978. 소수 찾기/소수 찾기.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+ 
+int main(void) {
+    int N, num, cnt = 0;
+    scanf("%d", &N);
+ 
+    int i;
+    while (N--) {
+        scanf("%d", &num);
+        //소수 검사
+        for (i = 2; i < num; i++) {
+            if (num%i == 0) break;
+        }
+        if (i == num) cnt++;
+    }
+    printf("%d\n", cnt);
+    return 0;
+}
